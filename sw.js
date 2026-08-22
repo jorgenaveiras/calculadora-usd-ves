67a2ec69ad17e0e701e3ff06e09b5437a54128cd
santobpm <santobpm@gmail.com>
2026-08-22 11:11:36 -0400

    Fix PWA paths for GitHub Pages subdirectory

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'calc-bcv-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/manifest.json'
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json'
 ];
 
 // Instalar
